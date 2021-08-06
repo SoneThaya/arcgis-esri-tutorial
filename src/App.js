0e7abb8f6095ea5756e08867ae2e3b53a7dc71ea
sone <sthaya@gmail.com>
2021-08-05 18:44:34 -0700

    started arcgis esri tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Map } from "@esri/react-arcgis";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        style={{ width: "100%", height: "100vh" }}
+        mapProperties={{ basemap: "satellite" }}
+        viewProperties={{
+          // center: [-122.4443, 47.2529],
+          //zoom: 3,
+          scale: 100000,
+        }}
+      />
     </div>
   );
 }
